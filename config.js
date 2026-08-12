0da778329e5e382bb9efe428ba53a4bd6f5d07c9
mela0976 <mela0976@users.noreply.github.com>
2026-08-12 15:37:29 +0800

    Configure Apps Script form endpoint

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Public website configuration. Never put Gmail passwords or LINE tokens here.
 window.MELA_CONFIG = Object.freeze({
-  formEndpoint: "",
+  formEndpoint: "https://script.google.com/macros/s/AKfycbyAa54n49u5407sRqRGtpqusVbGRPQE0QVTCpEnkdCpfA3c49Rdm6YIjP2DAjapRe3-Tg/exec",
   brandName: "MELA 系統顧問",
   responseTime: "1–2 個工作天"
 });
